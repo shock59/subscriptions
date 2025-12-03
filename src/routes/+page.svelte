efc84bce04c7096be975a6790115f0c62a614e89
shock59 <42130427+shock59@users.noreply.github.com>
2025-12-03 12:07:38 +1100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { resolve } from "$app/paths";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<p>
+  <a href={resolve("/auth")}>login</a>
+</p>

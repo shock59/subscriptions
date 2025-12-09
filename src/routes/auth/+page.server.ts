979388720314887ed84bd0f9c9c5158c214566cb
shock59 <42130427+shock59@users.noreply.github.com>
2025-12-09 13:39:15 +1100

    Remove /auth

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,31 +1,6 @@
-import * as auth from "$lib/server/auth";
-import { fail, redirect } from "@sveltejs/kit";
-import { getRequestEvent } from "$app/server";
-import type { Actions, PageServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-  const user = requireLogin();
-  return { user };
+  return redirect(302, "/");
 };
-
-export const actions: Actions = {
-  logout: async (event) => {
-    if (!event.locals.session) {
-      return fail(401);
-    }
-    await auth.invalidateSession(event.locals.session.id);
-    auth.deleteSessionTokenCookie(event);
-
-    return redirect(302, "/auth/login");
-  },
-};
-
-function requireLogin() {
-  const { locals } = getRequestEvent();
-
-  if (!locals.user) {
-    return redirect(302, "/auth/login");
-  }
-
-  return locals.user;
-}
